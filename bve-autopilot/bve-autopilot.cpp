b3254594ca494410cc26541357092f9aafca40cb
WATANABE Yuki <magicant@wonderwand.net>
2019-01-27 18:48:45 +0900

    ATS API を空の関数で定義

--- bve-autopilot/bve-autopilot.cpp
+++ bve-autopilot/bve-autopilot.cpp
@@ -19,4 +19,58 @@
 
 #include "stdafx.h"
 
+ATS_API int WINAPI GetPluginVersion() {
+    return ATS_VERSION;
+}
 
+ATS_API void WINAPI Load() {
+}
+
+ATS_API void WINAPI Dispose() {
+}
+
+ATS_API void WINAPI SetVehicleSpec(ATS_VEHICLESPEC spec) {
+}
+
+ATS_API void WINAPI Initialize(int brake) {
+}
+
+ATS_API ATS_HANDLES WINAPI Elapse(
+    ATS_VEHICLESTATE state, int *panelValues, int *soundStates) {
+    ATS_HANDLES handles;
+    handles.Brake = 0;
+    handles.Power = 0;
+    handles.Reverser = 0;
+    handles.ConstantSpeed = ATS_CONSTANTSPEED_CONTINUE;
+    return handles;
+}
+
+ATS_API void WINAPI SetPower(int notch) {
+}
+
+ATS_API void WINAPI SetBrake(int notch) {
+}
+
+ATS_API void WINAPI SetReverser(int notch) {
+}
+
+ATS_API void WINAPI KeyDown(int key) {
+}
+
+ATS_API void WINAPI KeyUp(int key) {
+}
+
+ATS_API void WINAPI HornBlow(int type) {
+}
+
+ATS_API void WINAPI DoorOpen() {
+}
+
+ATS_API void WINAPI DoorClose() {
+}
+
+ATS_API void WINAPI SetSignal(int aspect) {
+}
+
+ATS_API void WINAPI SetBeaconData(ATS_BEACONDATA data) {
+}
