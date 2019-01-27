0ce34a13c2c608481188ea6fa0810c318fd4529a
WATANABE Yuki <magicant@wonderwand.net>
2019-01-27 15:53:08 +0900

    ライセンス宣言を追加

--- bve-autopilot/stdafx.h
+++ bve-autopilot/stdafx.h
@@ -2,6 +2,22 @@
 // または、参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
 // を記述します。
 //
+// Copyright(C) 2019 Watanabe, Yuki
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110 - 1301  USA
 
 #pragma once
 
