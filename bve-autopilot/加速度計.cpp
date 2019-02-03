551649d1c8279cc57c45390a3357be08d72fb32d
WATANABE Yuki <magicant@wonderwand.net>
2019-02-03 16:55:24 +0900

    加速度計を追加

---
+++ bve-autopilot/加速度計.cpp
@@ -0,0 +1,67 @@
+// 加速度計.cpp : 速度変化から加速度を推定します
+//
+// Copyright © 2019 Watanabe, Yuki
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110 - 1301  USA
+
+#include "stdafx.h"
+#include "加速度計.h"
+
+namespace autopilot
+{
+
+    加速度計::加速度計()
+    {
+        リセット();
+    }
+
+
+    加速度計::~加速度計()
+    {
+    }
+
+    void 加速度計::リセット()
+    {
+        for (観測& 記録 : _記録) {
+            記録 = {};
+        }
+        _加速度 = {};
+    }
+
+    void 加速度計::経過(観測 今回)
+    {
+        // 過去"記録数"回分の記録との速度差から算出した加速度の平均を求める
+        加速度型 加速度和 = 0;
+
+        for (unsigned i = 0; i < 記録数 - 1; i++) {
+            時刻型 時刻差 = 今回._時刻 - _記録[i]._時刻;
+            速度型 速度差 = 今回._速度 - _記録[i]._速度;
+            if (時刻差 > 0) {
+                加速度和 += 速度差 / 時刻差;
+            }
+
+            if (i + 1 < 記録数) {
+                _記録[i] = _記録[i + 1];
+            }
+            else {
+                _記録[記録数 - 1] = 今回;
+            }
+        }
+
+        _加速度 = 加速度和 / 記録数;
+    }
+
+}
---
+++ bve-autopilot/加速度計.h
@@ -0,0 +1,50 @@
+// 加速度計.h : 速度変化から加速度を推定します
+//
+// Copyright © 2019 Watanabe, Yuki
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110 - 1301  USA
+
+#pragma once
+
+namespace autopilot
+{
+
+    class 加速度計
+    {
+    public:
+        using 加速度型 = double;
+        using 速度型 = double;
+        using 時刻型 = double;
+
+        struct 観測 {
+            速度型 _速度;
+            時刻型 _時刻;
+        };
+
+        加速度計();
+        ~加速度計();
+
+        void リセット();
+        void 経過(観測 データ);
+        加速度型 加速度() const { return _加速度; }
+
+    private:
+        constexpr static unsigned 記録数 = 3;
+        観測 _記録[記録数];
+        加速度型 _加速度;
+    };
+
+}
