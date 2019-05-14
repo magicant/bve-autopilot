873f41193375e735b6f3a01c4d4e1bbe204faa90
WATANABE Yuki <magicant@wonderwand.net>
2019-05-15 00:01:20 +0900

    制限グラフ: 出力ノッチで時間マージンを指定できるように

--- bve-autopilot/制限グラフ.h
+++ bve-autopilot/制限グラフ.h
@@ -47,7 +47,8 @@ namespace autopilot
         // 力行は正の値、制動は負の値
         int 出力ノッチ(
             距離型 現在位置, 速度型 現在速度, const 共通状態 & 状態,
-            速度型 速度マージン = mps_from_kmph(0.5)) const;
+            時間型 時間マージン = 2, 速度型 速度マージン = mps_from_kmph(0.5))
+            const;
 
     private:
         std::forward_list<制限区間> _区間リスト;
