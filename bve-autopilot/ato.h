1cb900ea4023bf76b7d5aede73f8b4dc60964266
WATANABE Yuki <magicant@wonderwand.net>
2019-11-07 11:34:41 +0900

    力行抑止状態を運転台パネルに出力

--- bve-autopilot/ato.h
+++ bve-autopilot/ato.h
@@ -52,6 +52,7 @@ namespace autopilot
         mps 現在制限速度(const 共通状態 &状態) const;
         mps 現在常用パターン速度(const 共通状態 &状態) const;
         mps 現在orp照査速度() const;
+        bool 力行抑止中() const { return _早着防止.出力ノッチ() <= 1; }
 
         // 力行は正の値、制動は負の値
         int 出力ノッチ() const { return _出力ノッチ; }
