98bf8a6dc0e31a167b70ef9d3e060f5c8a15485d
WATANABE Yuki <magicant@wonderwand.net>
2020-03-22 21:29:55 +0900

    勾配グラフ: 勾配区間から終点を削除
    dd9053a9 と同様のことを勾配グラフにも行う。
    
    ある区間の終点は次の区間の始点に等しいので
    データとして持たないようにする。

--- bve-autopilot/勾配グラフ.cpp
+++ bve-autopilot/勾配グラフ.cpp
@@ -21,6 +21,7 @@
 #include "勾配グラフ.h"
 #include <algorithm>
 #include <cmath>
+#include <iterator>
 #include "区間.h"
 
 #pragma warning(disable:4819)
@@ -37,13 +38,12 @@ namespace autopilot
 
     struct 勾配グラフ::勾配区間
     {
-        m 始点, 終点;
+        m 始点;
         double 勾配;
         mps2 影響加速度;
 
-        勾配区間(m 始点, m 終点, double 勾配) :
+        勾配区間(m 始点, double 勾配) :
             始点{始点},
-            終点{終点},
             勾配{勾配},
             影響加速度{-0.75 * 重力加速度 * 勾配} { }
         // 本当は tan を sin に変換すべきだがほとんど違わないので無視する
@@ -65,21 +65,21 @@ namespace autopilot
             return 区間.始点 >= 始点;
             });
 
-        // 新しい制限区間に重なる既存の区間を縮める
-        for (勾配区間 &区間 : _区間リスト) {
-            区間.終点 = std::min(区間.終点, 始点);
-        }
-
-        m 終点 = m::無限大();
-        _区間リスト.emplace_front(始点, 終点, 勾配);
+        _区間リスト.emplace_front(始点, 勾配);
     }
 
     void 勾配グラフ::通過(m 位置)
     {
         // 通過済みの区間を消す
-        _区間リスト.remove_if([位置](const 勾配区間 & 区間) {
-            return 区間.終点 < 位置;
-        });
+        auto i = _区間リスト.begin();
+        while (true) {
+            if (i == _区間リスト.end())
+                return;
+            if (i->始点 < 位置)
+                break;
+            ++i;
+        }
+        _区間リスト.erase_after(i, _区間リスト.end());
     }
 
     mps2 勾配グラフ::勾配加速度(区間 対象範囲) const
@@ -90,9 +90,11 @@ namespace autopilot
         }
 
         mps2 加速度 = 0.0_mps2;
+        auto 終点 = m::無限大();
         for (const 勾配区間 &区間 : _区間リスト) {
-            auto 影響区間 = 重なり({区間.始点, 区間.終点}, 対象範囲);
+            auto 影響区間 = 重なり({区間.始点, 終点}, 対象範囲);
             m 影響長さ = 影響区間.長さ();
+            終点 = 区間.始点;
             if (!(影響長さ > 0.0_m)) {
                 continue;
             }
