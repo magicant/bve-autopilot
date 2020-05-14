7c95ded88b65f14b3b01c222c2166217a1b2e560
WATANABE Yuki <magicant@wonderwand.net>
2020-05-14 21:12:23 +0900

    テストプロジェクトを追加

---
+++ bve-autopilot-test/bve-autopilot-test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace bveautopilottest
+{
+	TEST_CLASS(bveautopilottest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
