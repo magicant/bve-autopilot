81fb78b0524e3f7c2c4099d1e85933c9e5fbbb93
WATANABE Yuki <magicant@wonderwand.net>
2020-07-12 16:20:38 +0900

    tasc: 制御中及び転動防止ブレーキを解除する条件を変数化

--- bve-autopilot/tasc.h
+++ bve-autopilot/tasc.h
@@ -31,18 +31,36 @@ namespace autopilot {
     class tasc
     {
     public:
+        enum class イベント
+        {
+            なし,
+            停止,
+            戸開,
+            手動ブレーキ,
+        };
+        struct リセット条件 {
+            イベント タイミング;
+            s 遅延;
+        };
+
         tasc();
         ~tasc() = default;
 
         void リセット();
-        void 制動操作(const 共通状態 &状態);
+        void 制動操作(const 共通状態 &状態) {
+            手動ブレーキ条件ならイベント発動(状態);
+        }
+        void 戸開(const 共通状態 &状態) {
+            イベント発動(イベント::戸開, 状態.現在時刻());
+            手動ブレーキ条件ならイベント発動(状態);
+        }
         void 戸閉(const 共通状態 &状態);
         void 地上子通過(
             const ATS_BEACONDATA &地上子, m 直前位置, const 共通状態 &状態);
         void 経過(const 共通状態 & 状態);
 
         m 目標停止位置() const;
-        bool 制御中() const;
+        bool 制御中(時刻 現在時刻) const;
 
         自動制御指令 出力ノッチ() const noexcept { return _出力ノッチ; }
 
@@ -55,12 +73,15 @@ namespace autopilot {
         live<区間> _次駅停止位置のある範囲;
         m _調整した次駅停止位置;
         m _最大許容誤差;
-        bool _緩解;
+        リセット条件 _制御リセット条件, _緩解条件;
+        時刻 _制御リセット時刻, _緩解時刻;
         自動制御指令 _出力ノッチ;
 
         void 停止位置を追加(m 停止位置, const 共通状態 &状態);
         void 次駅停止位置を設定(m 残距離, m 直前位置, const 共通状態 &状態);
         void 最大許容誤差を設定(m 最大許容誤差);
+        void 手動ブレーキ条件ならイベント発動(const 共通状態 &状態);
+        void イベント発動(イベント イベント, 時刻 現在時刻);
     };
 
 }
